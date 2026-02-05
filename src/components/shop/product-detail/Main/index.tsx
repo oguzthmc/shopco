beea19c44da7584cf3e1b37b4ecd1127e859f57a
Oguzhan Tohumcu <47873038+oguzthmc@users.noreply.github.com>
2026-02-05 15:07:52 +0300

    fix: product main name & style fixes

--- src/components/shop/product-detail/Main/index.tsx
+++ src/components/shop/product-detail/Main/index.tsx
@@ -5,11 +5,11 @@ import ColorSelector from './ColorSelector';
 import SizeSelector from './SizeSelector';
 import AddToCartSection from './AddToCartSection';
 
-interface ProductDetailMainContentProps {
+interface ProductMainProps {
   data: Product;
 }
 
-const ProductDetailMainContent = ({ data }: ProductDetailMainContentProps) => {
+const ProductMain = ({ data }: ProductMainProps) => {
   return (
     <section className="mb-10 md:mb-20">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
@@ -23,16 +23,16 @@ const ProductDetailMainContent = ({ data }: ProductDetailMainContentProps) => {
           <ProductInfo data={data} />
 
           {/* Gelecek bileşenler için ayırıcılar ve placeholderlar */}
-          <hr className="h-px border-t-black/10 my-3 md:my-6" />
+          <hr className="h-px border-t-black/10 my-4 md:my-6" />
 
           {/* Renk Seçimi Bileşeni */}
           <ColorSelector />
 
-          <hr className="h-px border-t-black/10 my-3 md:my-6" />
+          <hr className="h-px border-t-black/10 my-4 md:my-6" />
 
           <SizeSelector />
 
-          <hr className="hidden md:block h-px border-t-black/10 my-3 md:my-6" />
+          <hr className="h-px border-t-black/10 my-4 md:my-6" />
 
           <AddToCartSection data={data} />
         </div>
@@ -41,4 +41,4 @@ const ProductDetailMainContent = ({ data }: ProductDetailMainContentProps) => {
   );
 };
 
-export default ProductDetailMainContent;
+export default ProductMain;
